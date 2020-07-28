0612c229f5fdaa281fe84295c23d256e6e63067c
Daniel Gould <dmgould310@gmail.com>
2020-07-28 19:46:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif string != string.upcase
-  return "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase
-  return "NO, NOT SINCE 1938!"
-  else 
-  "NO, NOT SINCE 1938!"
-  end
-end 
+    "I LOVE YOU TOO PUMPKIN!"
+end
